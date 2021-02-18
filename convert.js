f6d3e782106c966f69b8e5c2fbb8877ff33179e5
karex <iamkarex@gmail.com>
2021-02-18 02:01:07 +0100

    feat: reintroduce page maring for proper pagination margins, header rework, href style change

--- convert.js
+++ convert.js
@@ -30,4 +30,4 @@ while (freeFileName === false) {
     }
 }
 
-renderPDF.generateSinglePdf(currentPath + htmlFile, pdfFile, { chromeBinary: chromeBinary, noMargins: true, includeBackground: true});
+renderPDF.generateSinglePdf(currentPath + htmlFile, pdfFile, { chromeBinary: chromeBinary, noMargins: false, includeBackground: true});
